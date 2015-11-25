8879ab23ee52a391505da65077cee2642b715b58
tortoyoyo <tortoyoyo@gmail.com>
2015-11-25 11:12:36 -0200

    ignore agin

--- test/spec/TableListSpec.js
+++ test/spec/TableListSpec.js
@@ -1,7 +1,25 @@
+// describe("CadHistoriaEditor", function() {
+//   var $scope;
+//   beforeEach(function() {
+//     angular.mock.module('jogo');
+//     angular.mock.inject(function($injector) {
+//       $scope = $injector.get('$rootScope').$new();
+//     });
+//   });
+//   it("Deve criar um Historia fundo tem q ser vazio",
+//     angular.mock.inject(function($controller) {
+//       $controller('CadHistoriaEditor', {
+//         "$scope": $scope
+//       });
+//       expect($scope.historia.nomeQuadrinho).toBe("Sem nome");
+//     }));
+// });
+
+
 describe("IndexControler", function() {
   var $scope;
   beforeEach(function () {
-   module('conta-azul');
+   angular.mock.module('conta-azul');
 });
   // beforeEach(function() {
   //   angular.mock.module('conta-azul');
@@ -9,7 +27,7 @@ describe("IndexControler", function() {
   //     $scope = $injector.get('$rootScope').$new();
   //   });
   // });
-  it('greet deve retornar Hi', inject(function (GreetingService) {
-   expect(GreetingService.greeting()).toBe('Hi');
+  it('greet deve retornar Hi', angular.mock.inject(function (ListCarService) {
+   expect(ListCarService.labelTable()).toBe('Hi');
 }));
 });
